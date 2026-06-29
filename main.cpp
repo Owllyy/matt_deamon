e15dc8ffc3110b4cbc8692bcde99e77117fcbb5a
Lucas Bourret <bourretlucas@gmail.com>
2026-06-29 13:25:12 +0200

    fix(signal): There is no signal 0

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void signal_handler(int signum) {
 }
 
 void init_signals() {
-    for (int i = 0; i < NSIG; ++i) {
+    for (int i = 1; i < NSIG; ++i) {
         // Unix kernel does not allow to capture SIGKILL and SIGSTOP
         if (i == SIGKILL || i == SIGSTOP) continue;
         signal(i, signal_handler);
